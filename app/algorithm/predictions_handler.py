d05422cda09d0e40b910d6c9a95899e0e538a71a
Ahmed Karem Mousa <59775002+AhmedMousa-ag@users.noreply.github.com>
2023-02-12 21:15:53 +0200

    fixed a bug

--- app/algorithm/predictions_handler.py
+++ app/algorithm/predictions_handler.py
@@ -122,30 +122,24 @@ class Predictor:
         return predictions_response
 
     def conv_labels_no_probability(self, preds):
-        preds = np.array(tf.squeeze(preds))
-        if len(preds.shape) < 2:
-
-            if (
-                preds.size < 2
-            ):  # If passed one prediction it cause and error if not expanded dimention
-                prediction = np.array(
-                    tf.expand_dims(tf.round(preds), axis=0), dtype=int
-                )
+        #preds = np.array(tf.squeeze(preds))
+        if preds.shape[1] < 2:
+            preds = np.array(tf.squeeze(preds))
+            if preds.size < 2:  # If passed one prediction it cause and error if not expanded dimension
+                prediction = np.array(tf.expand_dims(
+                    tf.round(preds), axis=0), dtype=int)
             else:
                 prediction = np.array(tf.round(preds), dtype=int)
 
             return prediction
         else:
-
-            if (
-                preds.size < 2
-            ):  # If passed one prediction it cause and error if not expanded dimenstion
-                prediction = np.array(
-                    tf.expand_dims(tf.argmax(preds, axis=1), axis=0), dtype=int
-                )
+            if preds.size < 2:  # If passed one prediction it cause an error if not expanded dimenstion
+                prediction = np.array(tf.expand_dims(
+                    tf.argmax(preds,axis=1), axis=0), dtype=int)
+                
             else:
-                prediction = np.array(tf.argmax(preds, axis=1), dtype=int)
-
+                
+                prediction = np.array(tf.argmax(preds,axis=1), dtype=int)
             return prediction
 
     def save_predictions(self, save_path=SAVED_TEST_PRED_PATH):
